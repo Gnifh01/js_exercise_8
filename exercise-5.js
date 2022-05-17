3253383623968d81ad251fce759ea6681c8fc98c
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-05-17 16:18:58 +0200

    first commit

---
+++ exercise-5.js
@@ -0,0 +1,26 @@
+function calculateAverageAge(persons) {
+  let allAge = [];
+  for (let i = 0; i < persons.length; i++) {
+    allAge.push(persons[i].age);
+  }
+  let sum = allAge.reduce((sum, current) => sum + current, 0);
+  let media = sum / allAge.length;
+  return `La media di tutte le età è: ${media}!`;
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
